30467a75bc5f8b3145a8f1cb6b13df8ddd091264
JusticeForMikeBrown <quackmaster@protonmail.com>
2018-07-14 21:06:14 -0400

    newgpfsdir.py - fixed pep8 errors

--- newgpfsdir.py
+++ newgpfsdir.py
@@ -13,12 +13,14 @@ from slackclient import SlackClient
 support = 'help@dev.null'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('path',  help='location on gpfs filesystem where new directory will be created')
+parser.add_argument('path',  help='location on gpfs filesystem' +
+                                  ' where new directory will be created')
 parser.add_argument('directory',  help='directory to create under path')
 parser.add_argument('owner',  help='user that owns the new directory')
 parser.add_argument('group',  help='group that owns new directory')
-print("\nThis script allows authorized power users to create new directories under GPFS filesystems managed by the SCU.\n")
-print("For assistance contact", __author__, "or" , support +"." "\n")
+print("\nThis script allows authorized power users to create new" +
+      " directories under GPFS filesystems managed by the SCU.\n")
+print("For assistance contact", __author__, "or , support + ""." + "\n")
 args = parser.parse_args()
 
 # global vars
@@ -32,21 +34,26 @@ runas_user = os.environ['SUDO_USER']
 
 # slack variables
 #
-#token thus this script must not be readable by normal users 
+# token thus this script must not be readable by normal users
 # also do not send this variable into gitlab
-#slack_token = os.environ["QUOTA_TOKEN"]
+# slack_token = os.environ["QUOTA_TOKEN"]
 
 slack_token = "blahtoken"
 sc = SlackClient(slack_token)
 
-# slack messages sent 
+# slack messages sent
+
 msg = runas_user + ' created new directory ' + path + '/' + newdir
-emsg = runas_user + ' could NOT create directory ' + path + '/' + newdir + ' due error - please contact ' + support
+emsg1 = runas_user + ' could NOT create directory ' + path
+emsg2 = '/' + newdir + ' due error - please contact ' + support
+emsg = emsg1 + emsg2
 
 # list of power users and defined filesets where user quotas can be set
-lab = [ 'poweruser', '/gpfs/location/for/new/dir' ]
+lab = ['poweruser', '/gpfs/location/for/new/dir']
 
 # only allow valid path to be chosen
+
+
 def sanitize():
 
     if path == lab[1]:
@@ -55,19 +62,24 @@ def sanitize():
         print("You must choose a valid path.  Exiting...")
         sys.exit()
 
+
 sanitize()
+
 
 # ensure running this script on gpfs node
 def gpfs_node():
     try:
-        file = open('/var/adm/ras/mmfs.log.latest', 'r')
+        open('/var/adm/ras/mmfs.log.latest', 'r')
     except IOError:
         print("You must run this script on a GPFS node!")
         sys.exit()
 
-gpfs_node()    
+
+gpfs_node()
 
 # ensure user owner of newdir actually exists
+
+
 def checkuser():
     try:
         pwd.getpwnam(owner)
@@ -75,7 +87,9 @@ def checkuser():
         print("User", owner, "does not exist.")
         sys.exit()
 
+
 checkuser()
+
 
 # ensure group owner of newdir actually exists
 def checkgroup():
@@ -85,7 +99,9 @@ def checkgroup():
         print("Group", group, "does not exist.")
         sys.exit()
 
+
 checkgroup()
+
 
 def privs():
 
@@ -94,32 +110,27 @@ def privs():
     elif os.getuid() == 0:
         print("Creating new directory", newdir, "at location", path + "\n")
     else:
-        print("You must run with sudo!", "\n \n""Contact", support, "for assistance.")
+        print("You must run with sudo!", "\n \n""Contact", support)
         sys.exit()
+
+
 privs()
 
+
 def createdir():
-    
+
     ndir = path + '/' + newdir
 
     if not os.path.exists(ndir):
         os.makedirs(ndir)
         print("New directory created")
-        sc.api_call(
-         "chat.postMessage",
-          channel="#newgpfsdir",
-          text=msg
-         )
+        sc.api_call("chat.postMessage", channel="#newgpfsdir", text=msg)
     else:
         print(ndir + " already exists!")
-        sc.api_call(
-         "chat.postMessage",
-          channel="#newgpfsdir",
-          text=emsg
-         )
-    
+        sc.api_call("chat.postMessage", channel="#newgpfsdir", text=emsg)
+
     # set permissions
     shutil.chown(ndir, user=owner, group=group)
 
-createdir()
 
+createdir()
